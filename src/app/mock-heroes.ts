a9af77790e6ec4908e69eb2ec2ce8f8351e238ae
Charles-04 <charlesmir04@gmail.com>
2022-01-11 12:03:32 +0100

    worked on heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Emma' },
+  { id: 12, name: 'Peter' },
+  { id: 13, name: 'Rachael' },
+  { id: 14, name: 'Anna' },
+  { id: 15, name: 'Victor' },
+  { id: 16, name: 'Superman' },
+  { id: 17, name: 'Dynamo' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
